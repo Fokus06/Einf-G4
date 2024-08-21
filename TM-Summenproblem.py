f5037d7824ae450341ece9900d16c032e12ed016
Andrin Lerjen <andrin@lerjen.ch>
2024-08-21 16:33:00 +0200

    Teilerspeicherung funktioniert immer noch nicht

--- TM-Summenproblem.py
+++ TM-Summenproblem.py
@@ -2,6 +2,7 @@ array = [5, 34, 3, 12, 4, 2, 9, 23]
 zsum = 9
 nsel = []
 result = []
+zsave = 0
 
 for i in range(0,len(array)):
     az = array[i]
@@ -11,12 +12,13 @@ ksum = 0
 while ksum != zsum:
     for i in range(0,len(nsel)):
         if nsel[i] + ksum < zsum:
+            zsave = nsel[i]
             ksum += nsel[i]
         elif nsel[i] + ksum == zsum:
             ksum += nsel[i]
             print("fertig", result, ksum)
             exit(0)
-    #result.append(nsel[0])
+    result.append(zsave)
     nsel.remove(nsel[0])
     ksum = 0
 
