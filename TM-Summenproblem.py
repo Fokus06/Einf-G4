5f8f7e831b433125ea7f9b98f2bf7ea43aa07254
Andrin Lerjen <andrin@lerjen.ch>
2024-08-21 16:11:20 +0200

    TM-Summenproblem funktioniert, bis jetzt ohne Teilerspeicherung

---
+++ TM-Summenproblem.py
@@ -0,0 +1,22 @@
+array = [5, 34, 3, 12, 4, 2, 9, 23]
+zsum = 110
+nsel = []
+
+
+for i in range(0,len(array)):
+    az = array[i]
+    if az < zsum:
+        nsel.append(az)
+ksum = 0
+while ksum != zsum:
+    for i in range(0,len(nsel)):
+        if nsel[i] + ksum < zsum:
+            ksum += nsel[i]
+        elif nsel[i] + ksum == zsum:
+            ksum += nsel[i]
+            print("fertig", ksum)
+            exit(0)
+    nsel.remove(nsel[0])
+    ksum = 0
+
+
